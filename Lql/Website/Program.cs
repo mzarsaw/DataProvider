76ae133fb239fbdb0baae31b1a4257c1e83f8b65
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2025-08-09 20:03:37 +1000

    Attempt to fix website

--- Lql/Website/Program.cs
+++ Lql/Website/Program.cs
@@ -1,23 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LqlWebsite.Components;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
-}
-
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync().ConfigureAwait(false);
